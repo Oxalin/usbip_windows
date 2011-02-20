4221236f5b62adfb50673b07d6325efc900dff63
arjanmels <arjanmels@users.sourceforge.net>
2011-02-20 15:11:27 +0000

    Initial version fixed for x64 and as submitted for ReactOS driver signing

---
+++ tag/usbip_windows-0.1.0.0/i.bat
@@ -0,0 +1,9 @@
+
+rem bcdedit /set testsigning on
+
+certutil -enterprise -addstore Root USBIP_TestCert.cer
+certutil -enterprise -addstore TrustedPublisher USBIP_TestCert.cer
+
+cd output
+
+devcon install USBIPEnum.inf "root\USPIPEnum"
