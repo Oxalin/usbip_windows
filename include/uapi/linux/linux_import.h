1d982ac12b89080d9955dff494b4b88a89843a58
Alexandre Demers <alexandre.f.demers@gmail.com>
2018-04-12 19:55:26 -0400

    [common] Add some header files missing under MSVC (or earlier versions)
    Some files are imported partially or completely form Linux so we can share
    as much code as possible. That's the case for linux_import.h and usbip.h.
    
    stdint.h comes from Alexander Chemeris' GitHub repository:
    https://github.com/chemeris/msinttypes/blob/master/stdint.h
    
    These files will be used under both the userspace tools and the driver.
    
    Signed-off-by: Alexandre Demers <alexandre.f.demers@gmail.com>

---
+++ include/uapi/linux/linux_import.h
@@ -0,0 +1,13 @@
+// Since we want to talk with linux USB/IP module, we shall be able to
+// to understand what we are receiving. So here, we import some of the enum and
+// struct that will need to be translated from the linux module.
+
+// Imported from Linux USB include ch9.h
+enum usb_device_speed {
+	USB_SPEED_UNKNOWN = 0,                  /* enumerating */
+	USB_SPEED_LOW, USB_SPEED_FULL,          /* usb 1.1 */
+	USB_SPEED_HIGH,                         /* usb 2.0 */
+	USB_SPEED_WIRELESS,                     /* wireless (usb 2.5) */
+	USB_SPEED_SUPER,                        /* usb 3.0 */
+	USB_SPEED_SUPER_PLUS,                   /* usb 3.1 */
+};
